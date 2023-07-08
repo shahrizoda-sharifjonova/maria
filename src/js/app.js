1e45b3fb7d69204e4464798238f14689bc2723ef
shahriyor-sharifjonov <shahrizodadeveloping@gmail.com>
2023-07-08 17:27:24 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,30 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-// const swiper = new Swiper();
+const swiper = new Swiper();
+
+new Swiper(".reviews__swiper", {
+    grabCursor: true,
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    spaceBetween:30,
+    watchSlidesProgress: true,
+    navigation:{
+        prevEl: ".reviews__button-prev",
+        nextEl: ".reviews__button-next",
+    },
+    pagination:{
+        el: '.reviews__pagination',
+    },
+    breakpoints:{
+        575:{
+            slidesPerView: 3,
+            spaceBetween:30,
+        }
+    }
+});
 
 const menu = document.querySelector('.menu');
 const headerList = document.querySelector('.header__list');
